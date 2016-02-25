bf5b54290cacf83b9e2e5334eb551234061c7c71
Ryan J. Holland <RyanHolland1987@Gmail.com>
2016-02-25 01:46:10 -0600

    WIP author website

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	boxHeights();
+
+	$(window).resize(function() {
+		boxHeights();
+	})
+
+	function boxHeights() {
+
+		for (var box = 1; box < 5; box++) {
+			$('#box'+box).css('height', 'auto');
+		}
+
+		var heights = [
+		$('#box1').height(),
+		$('#box2').height(),
+		$('#box3').height(),
+		$('#box4').height()
+		];
+		var tallest = Math.max.apply(Math, heights);
+		for (var box = 1; box < 5; box++) {
+			$('#box'+box).height(tallest);
+		}
+	}
+
+})
